9c5cb02f2276f9ffa16c225379b2ddecdefebd3e
nilsw13 <nils.wenting@laplateforme.io>
2025-02-16 03:02:25 +0100

    add framer motion and animation on page loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 
 import './App.css'
 import DashBoard from './pages/DashBoard'
+import {motion, AnimatePresence} from 'framer-motion'
 
 function App() {
 
 
   return (
-    <>
-    <div id='app ' className='overflow-x-hidden p-4 ' >
+    <>    
+
+    <AnimatePresence>
+
+
+       <motion.div 
+        initial={{opacity : 0}}
+        animate={{opacity : 100}}
+        exit={{opacity : 0}}
+        transition={{duration : 3}}
+       id='app ' className='overflow-x-hidden p-4 ' >
 
           <div className='scale-90' >
+
+
           <DashBoard/>
           </div>
-      </div>
+      </motion.div>
+      </AnimatePresence>
     </>
   )
 }
