04c68c636a490ae371c0e99dc2e016475460d53b
nilsw13 <nils.wenting@laplateforme.io>
2025-02-17 00:48:44 +0100

    change structure of charts card

---
+++ src/components/dashboard/charts/ChartContainer.tsx
@@ -0,0 +1,56 @@
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+
+function ChartContainerComponent() {
+
+
+    const chartData = [
+        { month: "January", incomes: 186, expenses: 80 },
+        { month: "February", incomes: 305, expenses: 200 },
+        { month: "March", incomes: 237, expenses: 120 },
+        { month: "April", incomes: 73, expenses: 190 },
+        { month: "May", incomes: 209, expenses: 130 },
+        { month: "June", incomes: 214, expenses: 140 },
+      ]
+
+      const chartConfig = {
+        incomes: {
+          label: "incomes",
+          color: "var(--success-chart)",
+        },
+        expenses: {
+          label: "expenses",
+          color: "var(--destructive-chart)",
+        },
+      } satisfies ChartConfig
+
+
+  return (
+    <>
+            <ChartContainer config={chartConfig} className=' max-w-[100%] h-[180px]  max-h-[620px] mx-auto  rounded-md '>
+                        <BarChart accessibilityLayer data={chartData}>
+                          <CartesianGrid vertical={false} />
+                          <XAxis
+                            dataKey="month"
+                            tickLine={false}
+                            tickMargin={10}
+                            axisLine={false}
+                            tickFormatter={(value) => value.slice(0, 3)}
+                          />
+                          <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent indicator="dashed" />}
+                          />
+                                  <ChartLegend className='text-blue-300/60 group-hover:text-blue-300 transition-all h-full' content={<ChartLegendContent />} />
+                          <Bar dataKey="incomes" fill="var(--color-incomes)" radius={4} />
+                          <Bar dataKey="expenses" fill="var(--color-expenses)" radius={4} />
+                        </BarChart>
+                      </ChartContainer>
+    
+    </>
+  )
+}
+
+export default ChartContainerComponent
--- src/components/dashboard/charts/Charts.tsx
+++ src/components/dashboard/charts/Charts.tsx
@@ -1,31 +1,11 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
 import { AnimatePresence, motion } from 'framer-motion'
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+import ChartContainerComponent from './ChartContainer'
 
 function Charts() {
 
 
-    const chartData = [
-        { month: "January", incomes: 186, expenses: 80 },
-        { month: "February", incomes: 305, expenses: 200 },
-        { month: "March", incomes: 237, expenses: 120 },
-        { month: "April", incomes: 73, expenses: 190 },
-        { month: "May", incomes: 209, expenses: 130 },
-        { month: "June", incomes: 214, expenses: 140 },
-      ]
-
-      const chartConfig = {
-        incomes: {
-          label: "incomes",
-          color: "var(--success-chart)",
-        },
-        expenses: {
-          label: "expenses",
-          color: "var(--destructive-chart)",
-        },
-      } satisfies ChartConfig
-
+  
 
   return (
 
@@ -37,33 +17,31 @@ function Charts() {
            transition={{duration : 2, delay: 0.5}}
     >
 
-        <Card className=' bg-transparent border-white/20 border md:h-[655px] ' >
+        <Card className='w-full bg-transparent border-white/20 border md:h-[590px] group ' >
             <CardHeader>
-                <CardTitle className='font bold text-xl text-slate-100/90'>Spending / Incomes overview</CardTitle>
+                <CardTitle className='font bold text-xl text-blue-300/60 group-hover:text-blue-300 transition-all'>Spending / Incomes overview</CardTitle>
                 <CardDescription>
                     Showing total of incomes and spending for the last 6 months
                 </CardDescription>
             </CardHeader>
-            <CardContent>
-        <ChartContainer config={chartConfig} className='min-w-[90%] max-w-[100%] h-[300px] mt-20 max-h-[620px] mx-auto  rounded-md p-10'>
-          <BarChart accessibilityLayer data={chartData}>
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="month"
-              tickLine={false}
-              tickMargin={10}
-              axisLine={false}
-              tickFormatter={(value) => value.slice(0, 3)}
-            />
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent indicator="dashed" />}
-            />
-                    <ChartLegend className='text-white h-full' content={<ChartLegendContent />} />
-            <Bar dataKey="incomes" fill="var(--color-incomes)" radius={4} />
-            <Bar dataKey="expenses" fill="var(--color-expenses)" radius={4} />
-          </BarChart>
-        </ChartContainer>
+            <CardContent> 
+
+              <div className='mt-8 flex flex-col gap-10'>
+
+
+                    <div className='flex flex-col md:flex-row'>
+
+                     <ChartContainerComponent/>
+
+                     <ChartContainerComponent/>
+
+                      </div>
+
+
+                      <ChartContainerComponent/>
+
+              </div>
+
       </CardContent>
 
         </Card>
