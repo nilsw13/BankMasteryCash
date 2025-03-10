642e5618fa78e595f66bc2441560a1327df50a49
nilsw13 <nils.wenting@laplateforme.io>
2025-03-10 18:44:08 +0100

    add api url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,14 +1,15 @@
-import axios from "axios";
+import axios from 'axios'
 
-const apiUrl = "bankmasterycash-api-production.up.railway.app";
+const apiUrl = "https://bankmasterycash-api-production.up.railway.app";
 console.log("api url utilisé" + apiUrl);
 
 const api = axios.create({
-  baseURL: apiUrl || "http://localhost:8080",
-  headers: {
-    "X-Dashboard-Token": "dashboard_secret",
-    "Content-Type": "application/json",
-  },
-});
+    baseURL: apiUrl || 'http://localhost:8080',
+    headers: {
+        'X-Dashboard-Token' : 'dashboard_secret',
+        'Content-Type' : 'application/json',
+       
+    },
+})
 
 export default api;
